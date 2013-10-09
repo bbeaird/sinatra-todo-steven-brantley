d85f97c86ac76ed5ef5ea8d865eb046cbf429f45
Steven Nugent <stevennugentdev@gmail.com>
2013-10-08 20:40:48 -0700

    Update todo_app.rb

--- todo_app.rb
+++ todo_app.rb
@@ -2,13 +2,14 @@ $LOAD_PATH.unshift(File.expand_path('.'))
 
 require 'sinatra'
 require 'sinatra/activerecord'
-
 require 'models/todo'
 
-
+begin
 # This loads environment variables from the .env file
 require 'dotenv'
 Dotenv.load
+rescue LoadError
+end
 
 set :database, ENV['DATABASE_URL']
 
